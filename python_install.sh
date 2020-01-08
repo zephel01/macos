36abf13ba675d6dfec08d7cb24ec4542e07dd3fe
zephel01 <zephel01@gmail.com>
2020-01-08 19:01:57 +0900

    Update python_install.sh

--- python_install.sh
+++ python_install.sh
@@ -4,8 +4,7 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install pyenv
-brew install pyenv-virtualenv
+git clone git://github.com/yyuu/pyenv.git ~/.pyenv
 
 cat << 'EOF' >> ~/.bash_profile
 PYENV_ROOT=~/.pyenv
